d284a63196a95bc7574f009b7d0f88048ce8ef83
sspaeti <simon@ssp.sh>
2026-07-07 08:46:38 +0200

    update

--- main.go
+++ main.go
@@ -181,7 +181,7 @@ func subscribe(w http.ResponseWriter, r *http.Request, email, name string) {
 	}
 
 	plain(w, http.StatusOK,
-		"Almost there -> CHECK YOUR INBOX TO CONFIRM.\n\n"+
+		"Last step -> CONFIRM the email in your inbox from me (simon@ssp.sh).\n\n"+
 		"------\n\n"+
 			"Why subscribe? curl https://sub.ssp.sh/why\n")
 }
